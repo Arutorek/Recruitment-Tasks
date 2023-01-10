130b03a62a800e772af737fef06072262df8a2a0
Jakub Strzelecki <89380172+Arutorek@users.noreply.github.com>
2023-01-10 21:24:39 +0100

    Delete Main.java

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import stringCalculator.StringCalculator;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(StringCalculator.countString("10.-6-"));
-
-    }
-}
